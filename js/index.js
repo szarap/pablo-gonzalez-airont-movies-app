0ed3f979cd3c9f43a274fe1b501b9f4d6cad490b
Pablo Gonzalez <lic.szgonzalez@gmail.com>
2022-12-16 11:54:43 -0300

    new  commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/* import searchFilters from "../js/script";
+
+const d = document;
+d.addEventListener("DOMContentLoaded",(e)=>{
+    searchFilters(".search", ".search_list");
+}) */
--- js/script.js
+++ js/script.js
@@ -8,11 +8,12 @@ function fetchMovies() {
 /*         .then(resp =>resp.json()).then(data => console.log(data))
 }
 fetchMovies(); */
-let movies = [];
+/* ----------------------------------------------------- */
+/* let movies = [];
 const movieElement = document.querySelector("#search_list");
 
 function fetchMovies(){
-    fetch("https://api.themoviedb.org/3/search/keyword?api_key=38243ac70a30fecdb6afc3cf99a81c1b&page=1") 
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=38243ac70a30fecdb6afc3cf99a81c1b&query=${item}`) 
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
@@ -34,8 +35,48 @@ function loadData(data, element){
 
 
 
+fetchMovies(); */
+
+/* ------------------ */
+const d = document;
+const parameter = d.getElementById("search");
+const apiKey= '38243ac70a30fecdb6afc3cf99a81c1b';
+const httpCall = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${parameter}`);
+// q=${search} = search es ID que viene de un Id en el Html
+
+httpCall
+.then( resp => resp.json() )
+.then( data => {
+    const input = data.results;
+    console.log(data)
+    console.log(input);
+
+
+})
+.catch( console.warn ); 
 
 
 
+/* 
+httpCall
+    .then( resp => resp.json() )
+    .then( data => {
+        const url= data;
+        console.log(url)        
+    })
+    .catch( console.warn );
+    
+    const searchData = document.getElementById('') */
+/* return function searchFilters(input,selector){
+    d.addEventListener("keyup", (e)=>{
+        if(e.target.matches(input)){
+            if(e.key==="Escape") e.target.value="";
 
-fetchMovies();
+        d.querySelectorAll(selector).forEach((el)=>
+        el.textContent.toLowerCase().includes(e.target.value)
+        ?el.classList.remove("filter")
+        :el.classList.add("filter")
+        )
+        }
+    })
+} */
